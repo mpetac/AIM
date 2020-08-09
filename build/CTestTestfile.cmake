e72857f77dc00b83912f91217c2a0063e4062f08
dev <bitmediator@gmail.com>
2020-08-09 22:29:33 +0200

    no build and doc

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mpetac/Uni/Dev/MW/axi_cpp/axi_psdf
-# Build directory: /home/mpetac/Uni/Dev/MW/axi_cpp/axi_psdf/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
